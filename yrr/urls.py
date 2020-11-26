1b37dcea497c1b82f0e4c5e58b535183eb5e78ff
gtg7784 <tae.gun7784@gmail.com>
2020-11-27 04:51:03 +0900

    update

--- yrr/urls.py
+++ yrr/urls.py
@@ -3,7 +3,6 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url('/', views.index, name='index'),
     url('compare/', views.getResult),
     url('getall/', views.getAllProducts)
 ]
